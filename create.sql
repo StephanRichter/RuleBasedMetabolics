6154c7b82891d5e73c4b00a49ec34cdf0fb1fa98
Stephan Richter <srichter@srsoftware.de>
2013-10-02 17:01:44 +0200

    create updated

--- create.sql
+++ create.sql
@@ -2,8 +2,8 @@ USE rulebased;
 CREATE TABLE IF NOT EXISTS names 		(nid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,name TEXT NOT NULL);
 CREATE TABLE IF NOT EXISTS substances		(sid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,formula TEXT);
 CREATE TABLE IF NOT EXISTS parameters		(pid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,description TEXT NOT NULL);
-CREATE TABLE IF NOT EXISTS parameters_substances (def_sid INT NOT NULL, pid INT, abbrevation CHAR NOT NULL, specification TEXT,selector VARCHAR(10),ref_sid INT);
-CREATE TABLE IF NOT EXISTS names_substances	(nid INT NOT NULL,sid INT NOT NULL, PRIMARY KEY(nid,sid));
+CREATE TABLE IF NOT EXISTS parameters_substances(substance_sid INT NOT NULL, pid INT, abbrevation CHAR NOT NULL, specification TEXT,selector VARCHAR(10),refered_substance_sid INT);
+CREATE TABLE IF NOT EXISTS names_substances	(name_nid INT NOT NULL,substance_sid INT NOT NULL, PRIMARY KEY(name_nid,substance_sid));
 
 
 SHOW TABLES;

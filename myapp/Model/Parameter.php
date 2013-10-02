6675f808d891e5257f5170efdbcb1f8d0fd8d18f
Stephan Richter <srichter@srsoftware.de>
2013-10-02 16:43:43 +0200

    added model for Parameters

---
+++ myapp/Model/Parameter.php
@@ -0,0 +1,75 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Parameter Model
+ *
+ * @property Substance $Substance
+ */
+class Parameter extends AppModel {
+
+/**
+ * Primary key field
+ *
+ * @var string
+ */
+	public $primaryKey = 'pid';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'description';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'pid' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'description' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasAndBelongsToMany associations
+ *
+ * @var array
+ */
+	public $hasAndBelongsToMany = array(
+		'Substance' => array(
+			'className' => 'Substance',
+			'joinTable' => 'parameters_substances',
+			'foreignKey' => 'parameter_id',
+			'associationForeignKey' => 'substance_id',
+			'unique' => 'keepExisting',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+		)
+	);
+
+}

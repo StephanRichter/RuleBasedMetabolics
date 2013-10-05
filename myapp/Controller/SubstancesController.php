6f93ee457e06844580803d7f01bfb294b736b2de
Stephan Richter <srichter@srsoftware.de>
2013-10-05 21:49:15 +0200

    re-baked Substances-Controller, removed access to user.add method

--- myapp/Controller/SubstancesController.php
+++ myapp/Controller/SubstancesController.php
@@ -3,14 +3,107 @@ App::uses('AppController', 'Controller');
 /**
  * Substances Controller
  *
+ * @property Substance $Substance
+ * @property PaginatorComponent $Paginator
  */
 class SubstancesController extends AppController {
 
 /**
- * Scaffold
+ * Components
  *
- * @var mixed
+ * @var array
  */
-	public $scaffold;
+	public $components = array('Paginator');
 
-}
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Substance->recursive = 0;
+		$this->set('substances', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Substance->exists($id)) {
+			throw new NotFoundException(__('Invalid substance'));
+		}
+		$options = array('conditions' => array('Substance.' . $this->Substance->primaryKey => $id));
+		$this->set('substance', $this->Substance->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Substance->create();
+			if ($this->Substance->save($this->request->data)) {
+				$this->Session->setFlash(__('The substance has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The substance could not be saved. Please, try again.'));
+			}
+		}
+		$names = $this->Substance->Name->find('list');
+		$parameters = $this->Substance->Parameter->find('list');
+		$this->set(compact('names', 'parameters'));
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Substance->exists($id)) {
+			throw new NotFoundException(__('Invalid substance'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Substance->save($this->request->data)) {
+				$this->Session->setFlash(__('The substance has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The substance could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Substance.' . $this->Substance->primaryKey => $id));
+			$this->request->data = $this->Substance->find('first', $options);
+		}
+		$names = $this->Substance->Name->find('list');
+		$parameters = $this->Substance->Parameter->find('list');
+		$this->set(compact('names', 'parameters'));
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Substance->id = $id;
+		if (!$this->Substance->exists()) {
+			throw new NotFoundException(__('Invalid substance'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Substance->delete()) {
+			$this->Session->setFlash(__('The substance has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The substance could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}}

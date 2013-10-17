ac2fbc9d870ad34f3301c218488c01ea4dbb8cb9
Stephan Richter <srichter@srsoftware.de>
2013-10-17 11:11:34 +0200

    added Role model

---
+++ myapp/Model/Role.php
@@ -0,0 +1,24 @@
+<?php
+
+App::uses('AuthComponent', 'Controller/Component');
+
+class Role extends AppModel {
+	public $validate = array(
+			'role' => array(
+					'required' => array(
+							'rule' => array('notEmpty'),
+							'message' => 'A role name is mandatory'
+					),
+					'between' => array(
+							'rule'    => array('between', 5, 100),
+							'message' => 'Between 5 to 15 characters'
+					)
+			),
+			'description' => array(
+					'required' => array(
+							'rule' => array('notEmpty'),
+							'message' => 'A description for the role is required'
+					)
+			)
+	);
+}

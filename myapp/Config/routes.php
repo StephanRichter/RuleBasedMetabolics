36b14c9a061d37f36739fc635f5dc34274dfffba
Stephan Richter <github@srsoftware.de>
2013-10-29 17:17:45 +0100

    connected login and logout to respective users controller actions

--- myapp/Config/routes.php
+++ myapp/Config/routes.php
@@ -20,6 +20,8 @@
  */
 //	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 Router::connect('/',array('controller'=>'substances','action'=>'index'));
+Router::connect('/login',array('controller'=>'users','action'=>'login'));
+Router::connect('/logout',array('controller'=>'users','action'=>'logout'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
